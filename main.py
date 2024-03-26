b4f2cd54ffdcb4babc62e41cfff4042491a6a09e
Plakhotnik Alexandr <alexpl2003@mail.ru>
2024-03-26 14:25:41 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from tinkoff.invest import Client
+from tinkoff.invest.constants import INVEST_GRPC_API_SANDBOX
+
+load_dotenv()
+TOKEN = os.getenv("api_token")
+
+
+def connect_to_sandbox():
+    with Client(TOKEN, target=INVEST_GRPC_API_SANDBOX) as client:
+
+        print(client.users.get_accounts())
+
+
+if __name__ == '__main__':
+    connect_to_sandbox()
